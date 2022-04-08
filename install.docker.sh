96bcc399ce6860091fc6463c2e81243c2e6848fd
xinghuo1321 <51067840+xinghuo1321@users.noreply.github.com>
2022-04-08 08:40:15 +0800

    Update install.docker.sh
    Centos7.6

--- install.docker.sh
+++ install.docker.sh
@@ -3,16 +3,22 @@
 echo ''
 echo 'Installing dependencies...'
 echo ''
-sudo apt update -y && sudo apt upgrade -y
-sudo apt install -y docker.io
+sudo yum update -y && sudo yum upgrade -y
+sudo yum install -y docker
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 echo ''
+echo 'Start DOCKER'
+echo ''
+sudo systemctl start docker
+
+echo ''
 echo 'Configuring Docker...'
 echo ''
 sudo systemctl enable docker
+
 
 echo ''
 echo 'Installing app...'
